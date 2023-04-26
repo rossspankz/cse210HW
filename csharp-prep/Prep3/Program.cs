25ce7f0fb07bc3a1b06278af3203b21a894c05e1
Ross Showalter <131218858+rossspankz@users.noreply.github.com>
2023-04-26 16:55:28 -0400

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string answer;
+
+        do
+        {
+            
+        
+            Random randomGenerator = new Random();
+            int x = randomGenerator.Next(1,100);
+
+            Console.WriteLine("What is your guess? ");
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            if (x != z)
+            {     
+                if (x > z)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (x < z)
+                {
+                    Console.WriteLine("Lower");
+                }   
+            
+                do
+                {
+                    Console.WriteLine("What is your guess? ");
+                    z = Convert.ToInt32(Console.ReadLine());
+
+                    if (x > z)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+
+                    else if (x < z)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+
+                } while(x != z);
+            }
+            if (z == x)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            Console.WriteLine("Do you want to play again? ");
+            answer = Console.ReadLine();
+
+               
+
+        
+        }while (answer == "yes" || answer == "Yes" || answer == "Yeah" || answer == "Y" || answer == "y");
     }
 }
