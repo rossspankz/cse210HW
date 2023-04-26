f45c8cfa0196bd9a9552f4f7fd56a1f18281ff3c
Ross Showalter <131218858+rossspankz@users.noreply.github.com>
2023-04-26 16:56:12 -0400

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string x;
+        int y;
+        List<int> num = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        do
+        {
+            Console.WriteLine("Enter a number: "); x = Console.ReadLine();
+        y = int.Parse(x);
+        
+        if (y!=0)
+           { 
+            num.Add(y);
+           }
+
+        }while(y != 0);
+        
+        double sum = num.AsQueryable().Sum();
+        double av = Queryable.Average(num.AsQueryable());
+
+
+        Console.WriteLine("The sum is: "+sum); 
+        Console.WriteLine("The average is: "+av); 
+        Console.WriteLine("The Largest number is: " + num.Max()); 
+
+
     }
 }
